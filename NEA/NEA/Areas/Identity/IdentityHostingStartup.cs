788dbcaa15d8902e13981e7438b81e3d7d3c7523
OwenPauptit <55768469+OwenPauptit@users.noreply.github.com>
2020-08-13 13:19:52 +0100

    Scaffolded Identity
    Initial Commit
    
    Scaffolded Identify with overrides on Register and Manage/Index
    
    Added custom user data - First Name and Last Name
    
    Need to add roles next

---
+++ NEA/NEA/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NEA.Areas.Identity.Data;
+using NEA.Models;
+
+[assembly: HostingStartup(typeof(NEA.Areas.Identity.IdentityHostingStartup))]
+namespace NEA.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<NEAContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("NEAContextConnection")));
+
+                services.AddDefaultIdentity<NEAUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<NEAContext>();
+            });
+        }
+    }
+}
